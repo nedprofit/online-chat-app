5ecb9e9909810eb7e8a8dc8518b818609b883bda
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2023-12-23 17:58:13 +0300

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :authenticate_user!
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = current_user&.language || I18n.default_locale
+  end
 end
